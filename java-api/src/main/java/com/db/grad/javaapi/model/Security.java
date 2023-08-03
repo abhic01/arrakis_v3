df716e5f40a9c269d233f8e15bd272a90066c2a9
abhic01 <abhi19@vt.edu>
2023-08-03 14:28:06 +0100

    Fixed Security entity

--- java-api/src/main/java/com/db/grad/javaapi/model/Security.java
+++ java-api/src/main/java/com/db/grad/javaapi/model/Security.java
@@ -9,31 +9,36 @@ import javax.persistence.Table;
 @Table(name = "Security")
 public class Security {
     @Id
+    private int id;
     private String isin;
-    private String bond_currency;
     private String cusip;
-    private double face_value_mn;
     private String issuer_name;
-    private Date bond_maturity_date;
-    private String s_status;
-    private String s_type;
-    private double coupon_percent;
-    
+    private Date maturity_Date;
+    private float coupon;
+    private String type;
+    private float face_value;
+    private String currency;
+    private String status;
+
+    /*
+     * Getters and setters
+     */
+
     @Id
+    @Column(name = "id", nullable = false)
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
     @Column(name = "isin", nullable = false)
     public String getIsin() {
         return isin;
     }
     public void setIsin(String isin) {
         this.isin = isin;
-    }
-
-    @Column(name = "bond_currency", nullable = false)
-    public String getBond_currency() {
-        return bond_currency;
-    }
-    public void setBond_currency(String bond_currency) {
-        this.bond_currency = bond_currency;
     }
 
     @Column(name = "cusip", nullable = true)
@@ -44,14 +49,6 @@ public class Security {
         this.cusip = cusip;
     }
 
-    @Column(name = "face_value_mn", nullable = false)
-    public double getFace_value_mn() {
-        return face_value_mn;
-    }
-    public void setFace_value_mn(double face_value_mn) {
-        this.face_value_mn = face_value_mn;
-    }
-
     @Column(name = "issuer_name", nullable = false)
     public String getIssuer_name() {
         return issuer_name;
@@ -60,34 +57,53 @@ public class Security {
         this.issuer_name = issuer_name;
     }
 
-    @Column(name = "bond_maturity_date", nullable = false)
-    public Date getBond_maturity_date() {
-        return bond_maturity_date;
+    @Column(name = "maturity_date", nullable = false)
+    public Date getMaturity_Date() {
+        return maturity_Date;
     }
-    public void setBond_maturity_date(Date bond_maturity_date) {
-        this.bond_maturity_date = bond_maturity_date;
-    }
-
-    @Column(name = "s_status", nullable = false)
-    public String getS_status() {
-        return s_status;
-    }
-    public void setS_status(String s_status) {
-        this.s_status = s_status;
+    public void setMaturity_Date(Date maturity_Date) {
+        this.maturity_Date = maturity_Date;
     }
 
-    @Column(name = "s_type", nullable = false)
-    public String getS_type() {
-        return s_type;
+    @Column(name = "coupon", nullable = false)
+    public float getCoupon() {
+        return coupon;
     }
-    public void setS_type(String s_type) {
-        this.s_type = s_type;
+    public void setCoupon(float coupon) {
+        this.coupon = coupon;
     }
 
-    @Column(name = "coupon_percent", nullable = false)
-    public double getCoupon_percent() {
-        return coupon_percent;
+    @Column(name = "type", nullable = false)
+    public String getType() {
+        return type;
     }
-    public void setCoupon_percent(double coupon_percent) {
-        this.coupon_percent = coupon_percent;
+    public void setType(String type) {
+        this.type = type;
     }
+
+    @Column(name = "face_value", nullable = false)
+    public float getFace_value() {
+        return face_value;
+    }
+    public void setFace_value(float face_value) {
+        this.face_value = face_value;
+    }
+
+    @Column(name = "dog_id", nullable = false)
+    public String getCurrency() {
+        return currency;
+    }
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    @Column(name = "status", nullable = false)
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+
+}
