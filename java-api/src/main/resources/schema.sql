77bd97c529f9b98aabfb9173c9ad1bda45a1c0d9
dylan1742 <dylanmccarthy2013@icloud.com>
2023-08-03 21:11:17 +0100

    database updates expect security

--- java-api/src/main/resources/schema.sql
+++ java-api/src/main/resources/schema.sql
@@ -73,24 +73,24 @@
 DROP TABLE IF EXISTS Users;
 
 CREATE TABLE Users (
-   user_id INT PRIMARY KEY,
-   user_name VARCHAR(255),
-   user_email VARCHAR(255),
-   user_password VARCHAR(255)
+   id INT PRIMARY KEY,
+   'name' VARCHAR(255),
 );
 
 DROP TABLE IF EXISTS Book;
 
 CREATE TABLE Book (
-   book_id INT PRIMARY KEY,
-   book_name VARCHAR(255)
+   id INT PRIMARY KEY,
+   'name' VARCHAR(255),
+   email VARCHAR(255),
+   'role' VARCHAR(255);
 );
 
 DROP TABLE IF EXISTS Counter_Party;
 
 CREATE TABLE Counter_Party (
-   counter_party_id INT PRIMARY KEY,
-   counter_party_name VARCHAR(255)
+   id INT PRIMARY KEY,
+   'name' VARCHAR(255)
 );
 
 DROP TABLE IF EXISTS Security;
@@ -110,20 +110,21 @@ CREATE TABLE Security (
 DROP TABLE IF EXISTS Trade;
 
 CREATE TABLE Trade (
-   trade_id INT NOT NULL AUTO_INCREMENT,
-   trade_type VARCHAR(50),
-   trade_currency VARCHAR(4),
-   quantity INT,
-   trade_settlement_date DATE,
-   trade_status VARCHAR(50),
-   trade_date DATE,
-   unit_price DECIMAL(10, 2),
+   id INT NOT NULL AUTO_INCREMENT,
    book_id INT,
-   isin VARCHAR(255),
-   bond_holder VARCHAR(255),
-   PRIMARY KEY (trade_id),
-   FOREIGN KEY (book_id) REFERENCES Book(book_id),
-   FOREIGN KEY (isin) REFERENCES Security(isin)
+   security_id int,
+   counter_party_id int,
+   currency VARCHAR(4),
+   'status' VARCHAR(50),
+   quantity INT,
+   unit_price real,
+   buy_sell VARCHAR(50),
+   trade_date DATE,
+   settlement_date DATE,
+   PRIMARY KEY (id),
+   FOREIGN KEY (book_id) REFERENCES Book(id),
+   FOREIGN KEY (security_id) REFERENCES Security(id),
+   FOREIGN KEY (counter_party_id) REFERENCES Security(id),
 );
 
 DROP TABLE IF EXISTS Book_User;
@@ -132,6 +133,6 @@ CREATE TABLE Book_User (
    user_id INT,
    book_id INT,
    PRIMARY KEY (user_id, book_id),
-   FOREIGN KEY (user_id) REFERENCES Users(user_id),
-   FOREIGN KEY (book_id) REFERENCES Book(book_id)
+   FOREIGN KEY (user_id) REFERENCES Users(id),
+   FOREIGN KEY (book_id) REFERENCES Book(id)
 );
