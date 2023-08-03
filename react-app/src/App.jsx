55a0cd9ab9f5b41436f42a26d85cddae63c8cbc5
55gracelim <gl2495@nyu.edu>
2023-08-03 16:43:46 +0100

    firebase final

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,9 +1,36 @@
-import './App.css';
-import AllInfo from '../../react-app/src/components/bonds/AllInfo';
+import { Container, Row, Col } from "react-bootstrap";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import PhoneSignUp from "./components/PhoneSignUp";
+import ProtectedRoute from "./components/ProtectedRoute";
+import { UserAuthContextProvider } from "./context/userAuthContext";
 
 function App() {
   return (
-    <AllInfo/>
+    <Container style={{ width: "400px" }}>
+      <Row>
+        <Col>
+          <UserAuthContextProvider>
+            <Routes>
+              <Route
+                path="/home"
+                element={
+                  <ProtectedRoute>
+                    <Home />
+                  </ProtectedRoute>
+                }
+              />
+              <Route path="/" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+           <Route path="/phonesignup" element={<PhoneSignUp />} />
+            </Routes>
+          </UserAuthContextProvider>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
